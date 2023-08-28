f1434bd3fbb43b1b998caa81bc42eed77a923c30
Eli Richardson <elijahrich02@icloud.com>
2023-08-27 20:20:57 -0500

    copilot solution

--- src/main.ts
+++ src/main.ts
@@ -54,7 +54,103 @@ const sketch = (p5: P5) => {
   };
 
   const isRectColliding = (rect1: Rectangle, rect2: Rectangle): boolean => {
-    // what the hell
+    // determine if two rectangles are colliding
+    // make sure to account for canvas rotation and translation done by p5
+    // angle is in degrees
+    const rect1Points = [
+      { x: rect1.x - rect1.width / 2, y: rect1.y - rect1.height / 2 },
+      { x: rect1.x + rect1.width / 2, y: rect1.y - rect1.height / 2 },
+      { x: rect1.x + rect1.width / 2, y: rect1.y + rect1.height / 2 },
+      { x: rect1.x - rect1.width / 2, y: rect1.y + rect1.height / 2 },
+    ];
+    const rect2Points = [
+      { x: rect2.x - rect2.width / 2, y: rect2.y - rect2.height / 2 },
+      { x: rect2.x + rect2.width / 2, y: rect2.y - rect2.height / 2 },
+      { x: rect2.x + rect2.width / 2, y: rect2.y + rect2.height / 2 },
+      { x: rect2.x - rect2.width / 2, y: rect2.y + rect2.height / 2 },
+    ];
+
+    const rect1RotatedPoints = rect1Points.map(point => {
+      const x = point.x * p5.cos(rect1.angle) - point.y * p5.sin(rect1.angle) + rect1.x;
+      const y = point.x * p5.sin(rect1.angle) + point.y * p5.cos(rect1.angle) + rect1.y;
+      return { x, y };
+    });
+    const rect2RotatedPoints = rect2Points.map(point => {
+      const x = point.x * p5.cos(rect2.angle) - point.y * p5.sin(rect2.angle) + rect2.x;
+      const y = point.x * p5.sin(rect2.angle) + point.y * p5.cos(rect2.angle) + rect2.y;
+      return { x, y };
+    });
+    const rect1Edges = [
+      {
+        x1: rect1RotatedPoints[0].x,
+        y1: rect1RotatedPoints[0].y,
+        x2: rect1RotatedPoints[1].x,
+        y2: rect1RotatedPoints[1].y,
+      },
+      {
+        x1: rect1RotatedPoints[1].x,
+        y1: rect1RotatedPoints[1].y,
+        x2: rect1RotatedPoints[2].x,
+        y2: rect1RotatedPoints[2].y,
+      },
+      {
+        x1: rect1RotatedPoints[2].x,
+        y1: rect1RotatedPoints[2].y,
+        x2: rect1RotatedPoints[3].x,
+        y2: rect1RotatedPoints[3].y,
+      },
+      {
+        x1: rect1RotatedPoints[3].x,
+        y1: rect1RotatedPoints[3].y,
+        x2: rect1RotatedPoints[0].x,
+        y2: rect1RotatedPoints[0].y,
+      },
+    ];
+    const rect2Edges = [
+      {
+        x1: rect2RotatedPoints[0].x,
+        y1: rect2RotatedPoints[0].y,
+        x2: rect2RotatedPoints[1].x,
+        y2: rect2RotatedPoints[1].y,
+      },
+      {
+        x1: rect2RotatedPoints[1].x,
+        y1: rect2RotatedPoints[1].y,
+        x2: rect2RotatedPoints[2].x,
+        y2: rect2RotatedPoints[2].y,
+      },
+      {
+        x1: rect2RotatedPoints[2].x,
+        y1: rect2RotatedPoints[2].y,
+        x2: rect2RotatedPoints[3].x,
+        y2: rect2RotatedPoints[3].y,
+      },
+      {
+        x1: rect2RotatedPoints[3].x,
+        y1: rect2RotatedPoints[3].y,
+        x2: rect2RotatedPoints[0].x,
+        y2: rect2RotatedPoints[0].y,
+      },
+    ];
+    for (let i = 0; i < rect1Edges.length; i++) {
+      for (let j = 0; j < rect2Edges.length; j++) {
+        const x1 = rect1Edges[i].x1;
+        const y1 = rect1Edges[i].y1;
+        const x2 = rect1Edges[i].x2;
+        const y2 = rect1Edges[i].y2;
+        const x3 = rect2Edges[j].x1;
+        const y3 = rect2Edges[j].y1;
+        const x4 = rect2Edges[j].x2;
+        const y4 = rect2Edges[j].y2;
+        const denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+        const ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+        const ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+        if (ua >= 0 && ua <= 1 && ub >= 0 && ub <= 1) {
+          return true;
+        }
+      }
+    }
+    return false;
   };
 
   p5.setup = () => {
